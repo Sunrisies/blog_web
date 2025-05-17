0527081db9bd7794ae5a90e9e218c17b5281b7bf
朱重迁 <z32664206862022@163.com>
2025-05-17 10:36:45 +0800

    refactor(services): 优化请求类，添加拦截器和超时处理
    重构了 `Http` 类，增加了请求和响应拦截器功能，并引入了请求超时处理。同时，修复了 `post` 方法中请求体序列化的问题，提升了代码的可维护性和健壮性。

---
+++ src/types/index.tsx
@@ -0,0 +1,13 @@
+export interface IUser {
+    id: number;
+    public_id: string;
+    user_name: string;
+    pass_word: string;
+    image: string;
+    phone: string;
+    email: string;
+    created_at: string;
+    updated_at: string;
+    role: string;
+    permissions: number;
+}
