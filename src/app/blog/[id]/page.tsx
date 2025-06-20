30258cafca5111ae58d2a69aa978a403f0579451
朱重迁 <z32664206862022@163.com>
2025-06-20 17:07:19 +0800

    style: 统一代码格式，移除分号并优化导入语句

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,38 +1,38 @@
-import BlogDetail from "@/components/blog/blog-detail";
-import { ScrollToTopButton } from "@/components/ScrollToTopButton";
-import Http from "@/services/request";
-import { CategoriesType, IBlog } from "@/types/blog";
-import { getClientInfo } from "@/utils/get-client-info";
+import BlogDetail from "@/components/blog/blog-detail"
+import { ScrollToTopButton } from "@/components/ScrollToTopButton"
+import Http from "@/services/request"
+import { CategoriesType, IBlog } from "@/types/blog"
+import { getClientInfo } from "@/utils/get-client-info"
 
 // 获取博客仓库数据
-const getWarehouseApi = async <T,>() =>
-    await Http.get<T>("/article/uploadTime");
+// const getWarehouseApi = async <T,>() =>
+//     await Http.get<T>("/article/uploadTime");
 //获取分类数据
-const getCategoriesApi = async <T,>() => (await Http.get<T[]>("/categories")).data;
+const getCategoriesApi = async <T,>() => (await Http.get<T[]>("/categories")).data
 const getPostApi = async <T,>(slug: number) => {
     const headers = await getClientInfo()
-    return (await Http.get<T[]>(`/article?page=${slug}&limit=8`, { headers })).data;
+    return (await Http.get<T[]>(`/article?page=${slug}&limit=8`, { headers })).data
 }
 
 
 interface ArticleListProps {
-    params: Promise<{ id: string }>;
+    params: Promise<{ id: string }>
 }
 const Blog = async ({ params }: ArticleListProps) => {
-    const { id } = await params;
-    const { data: blog, pagination } = await getPostApi<IBlog>(+id);
-    const { data: categories } = await getCategoriesApi<CategoriesType>();
+    const { id } = await params
+    const { data: blog, pagination } = await getPostApi<IBlog>(+id)
+    const { data: categories } = await getCategoriesApi<CategoriesType>()
     return (
         <>
             <BlogDetail
-                blog={blog}
-                pagination={pagination!}
-                categories={categories}
-                id={+id}
+                blog={ blog }
+                pagination={ pagination! }
+                categories={ categories }
+                id={ +id }
             />
             <ScrollToTopButton />
         </>
-    );
-};
+    )
+}
 
-export default Blog;
+export default Blog
